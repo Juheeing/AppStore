7d9b886ebe64239ec67c908dd0ed72e590a634b4
김주희 <juhee1687@naver.com>
2024-09-07 11:52:23 +0900

    init

---
+++ AppStore/AppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreApp.swift
+//  AppStore
+//
+//  Created by 김주희 on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
